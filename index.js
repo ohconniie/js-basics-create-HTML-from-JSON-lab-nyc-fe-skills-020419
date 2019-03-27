0706e04fe7bf319f8742975725dc23df72a5381f
Connie Chan <conniechan.10@gmail.com>
2019-03-27 23:04:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
+  // 1 Find out which button they pressed
+  // 2 Get data for that movie
+  // 3 Replace the template values with the real ones
 });
