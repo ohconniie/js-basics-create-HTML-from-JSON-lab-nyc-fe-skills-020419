954756edccd07543f728007ed66f47daf638cf5a
Connie Chan <conniechan.10@gmail.com>
2019-03-28 00:31:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", (e) => {
+// document.addEventListener("DOMContentLoaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
   // 1 Find out which button they pressed
@@ -51,4 +51,4 @@ document.addEventListener("DOMContentLoaded", (e) => {
   
   titanicButton.addEventListener('click', () => onMovieChange('Titanic'))
   terminatorButton.addEventListener('click', () => onMovieChange('Terminator 2'))
-});
+// });
